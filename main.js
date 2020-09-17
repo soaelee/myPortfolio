144b2b4285cd880bd0b594ec60bb615022b2579d
soaelee <soae0923@gmail.com>
2020-09-17 09:50:35 +0900

    JS: navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--color");
+  } else {
+    navbar.classList.remove("navbar--color");
+  }
+});
