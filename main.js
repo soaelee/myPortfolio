85f7a6df9d372978fad1ecac1ddf3d2084dfcbdf
soaelee <soae0923@gmail.com>
2020-09-17 12:39:22 +0900

    JS: navbar menu click

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 "use strict";
-// Make navbar transparent when it is on the top
+
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
+const navbarMenu = document.querySelector(".navbar__menu");
+// Make navbar transparent when it is on the top
 
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
@@ -9,4 +11,16 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--color");
   }
+});
+
+//Handle scrolling when tapping on the navbar menu
+
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
